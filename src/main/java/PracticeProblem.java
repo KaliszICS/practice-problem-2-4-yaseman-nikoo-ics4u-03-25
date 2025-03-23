5e5be00a292b3fd328c8e679209de034d9e2d92b
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-03-23 23:53:30 +0000

    ,

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,39 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static void selectionSortName(String[] name, int[] ages){
+		int smallest=0;
+		for (int i=0; i<name.length-1;i++){
+			for (int j=0; j<name.length;i++){
+				if(name[j].toLowerCase().compareTo(name[j+1].toLowerCase())>0){
+					String temp=name[smallest];
+					name[smallest]=name[j];
+					name[j]=temp;
+					int tempr=ages[smallest];
+					ages[smallest]=ages[j];
+					ages[j]=tempr;
 
-	
+				}
+			}
+		}
+
+	}
+	public static void selectionSortAge(String[] name, int[] ages){
+		int smallest=0;
+		for (int i=0; i<name.length-1;i++){
+			for (int j=0; j<name.length;i++){
+				if(ages[smallest]>ages[j]){
+					String temp=name[smallest];
+					name[smallest]=name[j];
+					name[j]=temp;
+					int tempr=ages[smallest];
+					ages[smallest]=ages[j];
+					ages[j]=tempr;
+
+				}
+			}
+		}
+
+	}
 
 }
