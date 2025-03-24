4797ae20afa2882dfded6085a9018c9bb6042115
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-03-24 19:20:48 +0000

    cmplete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,39 +1,44 @@
+
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 	public static void selectionSortName(String[] name, int[] ages){
-		int smallest=0;
 		for (int i=0; i<name.length-1;i++){
-			for (int j=0; j<name.length;i++){
-				if(name[j].toLowerCase().compareTo(name[j+1].toLowerCase())>0){
-					String temp=name[smallest];
-					name[smallest]=name[j];
-					name[j]=temp;
-					int tempr=ages[smallest];
-					ages[smallest]=ages[j];
-					ages[j]=tempr;
+			int smallest=i;
+			for (int j=i+1; j<name.length;j++){
+				if(name[j].toLowerCase().compareTo(name[smallest].toLowerCase()) < 0){
+					smallest=j;
 
 				}
 			}
+				String temp=name[smallest];
+					name[smallest]=name[i];
+					name[i]=temp;
+					int tempr=ages[smallest];
+					ages[smallest]=ages[i];
+					ages[i]=tempr;
+			
 		}
 
 	}
 	public static void selectionSortAge(String[] name, int[] ages){
 		int smallest=0;
-		for (int i=0; i<name.length-1;i++){
-			for (int j=0; j<name.length;i++){
+		for (int i=0; i<ages.length-1;i++){
+			for (int j=i+1; j<ages.length;j++){
 				if(ages[smallest]>ages[j]){
-					String temp=name[smallest];
-					name[smallest]=name[j];
-					name[j]=temp;
-					int tempr=ages[smallest];
-					ages[smallest]=ages[j];
-					ages[j]=tempr;
-
+					smallest=j;
 				}
 			}
+				String temp=name[smallest];
+				name[smallest]=name[i];
+				name[i]=temp;
+				int tempr=ages[smallest];
+				ages[smallest]=ages[i];
+				ages[i]=tempr;
+
 		}
 
 	}
